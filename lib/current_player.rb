bebfcb49561e0ba520583103efa491eac281e243
Martin Moy <martinmoy1991@gmail.com>
2020-03-27 16:01:30 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,1 @@
-def turn_count(board)
-  turn_count = 0
-  board.each do |value|
-    if value == "X" || value == "O"
-      turn_count += 1
-    end
-  end
-  return turn_count
-end
-
-def current_player(board)
-  turn_count(board).even? ? "X": "O"
-end
+def turn_count
