0d698d7f31068b6e389ed121d6e12c56c17ca1fe
Martin Moy <martinmoy1991@gmail.com>
2020-03-27 16:12:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,10 +9,13 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board).even?
-    return "X"
-  else
-    return "O"
-  end
-
+  turn_count(board).even? ? "X": "O"
 end
+#
+#def current_player(board)
+#  if turn_count(board).even?
+#    return "X"
+#  else
+#    return "O"
+#  end
+#end
